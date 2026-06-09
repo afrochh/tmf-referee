cac2b1fcb2f233ac0522142542e37e6ca9c556f2
afrochh <afrauz@outlook.com>
2026-06-09 04:33:02 +0300

    PWA eklendi

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE = "tmf-v1";
+const FILES = [
+  "/tmf-referee/",
+  "/tmf-referee/index.html",
+  "/tmf-referee/day2.js",
+  "/tmf-referee/assets/audio/kayit_1.mp3",
+  "/tmf-referee/assets/audio/kayit_1_short.mp3",
+  "/tmf-referee/assets/audio/kayit_2.mp3",
+  "/tmf-referee/assets/audio/kayit_2_short.mp3",
+  "/tmf-referee/assets/audio/kayit_3.mp3",
+  "/tmf-referee/assets/audio/kayit_3_short.mp3",
+  "/tmf-referee/assets/audio/kayit_4.mp3",
+  "/tmf-referee/assets/audio/kayit_4_short.mp3",
+  "/tmf-referee/assets/audio/kayit_5.mp3",
+  "/tmf-referee/assets/audio/kayit_5_short.mp3",
+  "/tmf-referee/assets/audio/kayit_6.mp3",
+  "/tmf-referee/assets/audio/kayit_6_short.mp3",
+  "/tmf-referee/assets/audio/kayit_7.mp3",
+  "/tmf-referee/assets/audio/kayit_7_short.mp3",
+  "/tmf-referee/assets/audio/kayit_8.mp3",
+  "/tmf-referee/assets/audio/kayit_8_short.mp3",
+  "/tmf-referee/assets/audio/kayit_9.mp3",
+  "/tmf-referee/assets/audio/kayit_9_short.mp3",
+  "/tmf-referee/assets/audio/kayit_10.mp3",
+  "/tmf-referee/assets/audio/kayit_10_short.mp3",
+  "/tmf-referee/assets/images/favicon.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
